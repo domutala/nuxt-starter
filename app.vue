e2bd072aa099fa630adadfda924769bd1b709d5a
domutala <ibntalla@gmail.com>
2023-11-03 10:29:44 +0100

    init plugin

--- app.vue
+++ app.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-const { result, search } = useAlgoliaSearch('articles')
+const { result, search } = useAlgoliaSearch("articles");
 
 onMounted(async () => {
-  await search({ query: 'techno' })
-})
+  await search({ query: "techno" });
+});
 </script>
 
 <template>
   <div>
-    <!-- <div>{{ result }}</div> -->
-    <NuxtWelcome />
+    <div>{{ result }}</div>
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,16 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/algolia', '@nuxtjs/tailwindcss'],
 
+  tailwindcss: {
+    cssPath: '~/styles/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  },
+
   algolia: {
     apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
     applicationId: process.env.ALGOLIA_APPLICATION_ID,
